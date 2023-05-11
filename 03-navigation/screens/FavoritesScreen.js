47cb1d9a571a8157c5a35333ad5ba22f87f04eb0
Ioannis Merianos <ioannismeri@gmail.com>
2023-05-11 19:29:37 +0300

    add drawer navigation and stack navigation

---
+++ 03-navigation/screens/FavoritesScreen.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Text } from "react-native";
+
+function FavoritesScreen() {
+  return <Text>{`The favorites screen!`}</Text>;
+}
+
+export default FavoritesScreen;
