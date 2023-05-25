ad9ad938d33def0073f942ca1512b7d318050449
Ioannis Merianos <ioannismeri@gmail.com>
2023-05-25 21:18:43 +0300

    navigating programmatically between screens

--- 05-expenses-tracker/App.js
+++ 05-expenses-tracker/App.js
@@ -15,7 +15,7 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
   return (
     <BottomTabs.Navigator
-      screenOptions={{
+      screenOptions={({ navigation }) => ({
         headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         headerTintColor: "white",
         tabBarStyle: {
@@ -27,10 +27,12 @@ function ExpensesOverview() {
             icon="add"
             size={24}
             color={tintColor}
-            onPress={() => {}}
+            onPress={() => {
+              navigation.navigate("ManageExpense");
+            }}
           />
         ),
-      }}
+      })}
     >
       <BottomTabs.Screen
         name="RecentExpenses"
