9c7059918e3e290f00e3ccc7d26e6c8a31538950
Ioannis Merianos <ioannismeri@gmail.com>
2023-05-31 13:40:27 +0300

    using context from inside components

--- 05-expenses-tracker/screens/RecentExpenses.js
+++ 05-expenses-tracker/screens/RecentExpenses.js
@@ -1,7 +1,21 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function RecentExpenses() {
-  return <Text>RecentExpenses Screen</Text>;
+  const expensesCtx = useContext(ExpensesContext);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+
+  return (
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod={`Last 7 Days`} />
+  );
 }
 
 export default RecentExpenses;
