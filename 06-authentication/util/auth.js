6536f38a03ff92f0931e44664ce9c5e224bcff26
Ioannis Merianos <ioannismeri@gmail.com>
2023-06-20 20:07:52 +0300

    creating new users

--- 06-authentication/util/auth.js
+++ 06-authentication/util/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyC4tW9vSDvkRvnu7EFzwQGDmsY-cEj_Jbs";
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=" +
       API_KEY,
