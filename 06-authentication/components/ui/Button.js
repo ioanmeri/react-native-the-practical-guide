2e21a63f59d9387acc77855276b5a96d6dddd0e2
Ioannis Merianos <ioannismeri@gmail.com>
2023-07-12 22:09:28 +0300

    styling place items

--- 06-authentication/components/ui/Button.js
+++ 06-authentication/components/ui/Button.js
@@ -1,6 +1,6 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-import { Colors } from '../../constants/styles';
+import { Colors } from "../../constants/styles";
 
 function Button({ children, onPress }) {
   return (
@@ -19,23 +19,23 @@ export default Button;
 
 const styles = StyleSheet.create({
   button: {
-    borderRadius: 6,
     paddingVertical: 6,
     paddingHorizontal: 12,
     backgroundColor: Colors.primary500,
     elevation: 2,
-    shadowColor: 'black',
-    shadowOffset: { width: 1, height: 1 },
+    shadowColor: "black",
     shadowOpacity: 0.25,
-    shadowRadius: 4,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+    borderRadius: 4,
   },
   pressed: {
     opacity: 0.7,
   },
   buttonText: {
-    textAlign: 'center',
-    color: 'white',
+    textAlign: "center",
+    color: "white",
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
 });
